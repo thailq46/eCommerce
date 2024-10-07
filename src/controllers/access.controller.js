9c5d3ad2975e799c8208175064535d0414b1449c
Le Quang Thai <xinphatmutikill@gmail.com>
2024-10-07 22:05:41 +0700

    Make Your API Response use class

--- src/controllers/access.controller.js
+++ src/controllers/access.controller.js
@@ -1,10 +1,17 @@
 "use strict";
+const {CREATED} = require("../core/success.response");
 const AccessService = require("../services/access.service");
 
 class AccessController {
    signUp = async (req, res, next) => {
       console.log("[P]::SignUp::", req.body);
-      return res.status(201).json(await AccessService.signUp(req.body));
+      new CREATED({
+         message: "Register successfully OK!",
+         metadata: await AccessService.signUp(req.body),
+         options: {
+            limit: 10,
+         },
+      }).send(res);
    };
 }
 
---
+++ src/core/success.response.js
@@ -0,0 +1,42 @@
+"use strict";
+const {ReasonStatus, StatusCode} = require("../utils/constants");
+
+class SuccessResponse {
+   constructor({
+      message,
+      statusCode = StatusCode.OK,
+      reasonStatusCode = ReasonStatus.OK,
+      metadata = {},
+   }) {
+      this.message = !message ? reasonStatusCode : message;
+      this.status = statusCode;
+      this.metadata = metadata;
+   }
+
+   send(res, headers = {}) {
+      return res.status(this.status).json(this);
+   }
+}
+
+class OK extends SuccessResponse {
+   constructor({message, metadata}) {
+      super({message, metadata});
+   }
+}
+class CREATED extends SuccessResponse {
+   constructor({
+      options = {},
+      message,
+      statusCode = StatusCode.CREATED,
+      reasonStatusCode = ReasonStatus.CREATED,
+      metadata,
+   }) {
+      super({message, statusCode, reasonStatusCode, metadata});
+      this.options = options;
+   }
+}
+
+module.exports = {
+   OK,
+   CREATED,
+};
